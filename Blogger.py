12585d4fddc930fe7bf11c6b094fa1d4e3d1df21
abdo-giprel <abdoheka12@gmail.com>
2020-04-27 19:30:21 +0200

    luanching first home page with python

---
+++ Blogger.py
@@ -0,0 +1,16 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def hello():
+    return "<h1>HOME PAGE</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>ABOUT PAGE</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
