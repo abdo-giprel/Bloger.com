3dda1cab719afaa25b63a4a9accc6d26273dde18
abdo-giprel <abdoheka12@gmail.com>
2020-04-28 21:29:40 +0200

    second section flask series

--- Blogger.py
+++ Blogger.py
@@ -1,15 +1,23 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template as view,url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'ABDO giprel',
+        'title': 'world',
+        'content': 'first post content',
+        'date_posted': 'April 21,2018'
+    }
+]
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>HOME PAGE</h1>"
+    return view('home.html',posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>ABOUT PAGE</h1>"
+    return view('about.html',title='About')
 
 
 if __name__ == '__main__':
